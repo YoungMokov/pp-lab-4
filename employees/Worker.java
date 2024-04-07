b29eaae9080cbc149af6006b7de9bd2b7e260952
Mikołaj Romanowski <84807601+YoungMokov@users.noreply.github.com>
2024-04-07 10:18:10 +0200

    Add files via upload

---
+++ employees/Worker.java
@@ -0,0 +1,13 @@
+package employees;
+
+import folder.Employee;
+
+public class Worker extends Employee {
+    
+    public String getPosition(double position) {
+        return  position;
+    
+        public String toString(String fullName, String salary )  {
+            return "Worker: " + getFullName + ", Salary: " + getSalary + "Psition" + position;
+        }
+}
